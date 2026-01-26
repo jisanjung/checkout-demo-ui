13d1f7d04096cb61be50ea9a759e1797b3a491f2
Jisan Jung <jjjung13@gmail.com>
2026-01-25 22:05:45 -0600

    get stripe create checkout session call working

--- app/api/checkout-sessions-create/route.ts
+++ app/api/checkout-sessions-create/route.ts
@@ -1,6 +1,18 @@
 
-export const GET = async (request: Request) => {
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '');
+
+export const POST = async (req: Request) => {
+    const body = await req.json();
+    const session = await stripe.checkout.sessions.create({
+        success_url: 'https://example.com/success',
+        cancel_url: 'https://example.com/cancel',
+        line_items: body,
+        mode: 'payment',
+    });
+    
     return new Response(JSON.stringify({
         success: true,
+        url: session?.url,
     }));
 };
