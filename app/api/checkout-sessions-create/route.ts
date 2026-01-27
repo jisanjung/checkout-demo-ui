8ff304b9054ddfb5ce8929143779c5b80bc50002
Jisan Jung <jjjung13@gmail.com>
2026-01-26 18:51:28 -0600

    error handling on api post

--- app/api/checkout-sessions-create/route.ts
+++ app/api/checkout-sessions-create/route.ts
@@ -3,16 +3,22 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '');
 
 export const POST = async (req: Request) => {
-    const body = await req.json();
-    const session = await stripe.checkout.sessions.create({
-        success_url: 'https://example.com/success',
-        cancel_url: 'https://example.com/cancel',
-        line_items: body,
-        mode: 'payment',
-    });
+    try {
+        const body = await req.json();
+        const session = await stripe.checkout.sessions.create({
+            success_url: 'https://example.com/success',
+            cancel_url: 'https://example.com/cancel',
+            line_items: body,
+            mode: 'payment',
+        });
     
-    return new Response(JSON.stringify({
-        success: true,
-        url: session?.url,
-    }));
+        return new Response(JSON.stringify({
+            url: session?.url,
+        }));
+    } catch (error) {
+        return new Response(JSON.stringify({
+            error: true,
+            message: 'ERROR_CHECKOUT_SESSIONS_CREATE_POST'
+        }));
+    }
 };
